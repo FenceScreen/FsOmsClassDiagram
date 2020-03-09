0af2d7a1f26c9786a6a62f2e60a9b2742285a0fb
Paul Barton <paulb33@gmail.com>
2020-03-09 10:17:55 -0700

    Added width and hei\ht in feet and inches (int)

--- FsOmsClassDiagram/Panel.cs
+++ FsOmsClassDiagram/Panel.cs
@@ -3,9 +3,11 @@
 namespace FsOmsClassDiagram {
   public class Panel {
     public int Id { get; set; }
-    public string  Width { get; set; }
-    public string Height { get; set; }
-    public string Dimentions => $"{Width}x{Height}";
+    public int  WidthFeet { get; set; }
+    public int WidthInches { get; set; }
+    public int HeightFeet { get; set; }
+    public int HeightInches { get; set; }
+    public string Dimensions => $"{WidthFeet}'-{WidthInches}\" x {HeightFeet}'-{HeightInches}\"";
     public int GrommetSpacing { get; set; }
     public string FilePath { get; set; }
     public string Description { get; set; }
